4e79e9a9a183b0e326addd4c2f3bb87503832c0f
Pushpa <Pushpa@Pushpa-PC>
2017-11-20 11:24:08 +0530

    check the treemap

--- CorejavaInterview/src/com/or/overriding/ExceptionEx.java
+++ CorejavaInterview/src/com/or/overriding/ExceptionEx.java
@@ -1,23 +1,15 @@
 package com.or.overriding;
 
-import java.util.logging.Logger;
+import java.util.TreeMap;
 
 public class ExceptionEx {
 
 	public static void main(String[] args) {
-		System.out.println("55");
-		System.out.println("55");
-		System.out.println("55");
-		System.out.println("gfhjgh");
-		System.out.println("55");
-		try{
-			System.out.println(10/0);
-		}catch (ArithmeticException e) {
-			System.out.println(10/2);
-		}
+		
+		TreeMap t= new TreeMap<>();
+		t.put(null, null);
+		System.out.println(t);
 		System.out.println("fghj");
-		System.out.println("tyui");
-		System.out.println("gfd");
 	}
 
 }
